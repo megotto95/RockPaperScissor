acc2836195b49845a1a21937599d70f80392084a
megotto95 <megotto95@gmail.com>
2023-08-10 22:16:48 -0400

    Change random number expression and add first if
    Change multiplier to 10 and created first if of >= 7

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function getComputerChoice() {
-    let x = Math.floor(Math.random() * 100)
+    let x = Math.floor(Math.random() * 10)
     console.log(x)
+    let comResult;
+    if (x >= 7) {
+        comResult = "rock"
+        return comResult
+    }
+    
     }
 
 
 new1 = getComputerChoice ()
+console.log(new1)
