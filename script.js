09952ac6f6480edc50e2f81cc9aceec3b6531f7f
megotto95 <megotto95@gmail.com>
2023-08-10 23:35:55 -0400

    Create game function with for loop
    for loop initiated

--- script.js
+++ script.js
@@ -22,19 +22,70 @@ function getComputerChoice() {
 const computerSelection = getComputerChoice ()
 console.log(computerSelection)
 const playerSelection = "rock"
+
 function round(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
     if (playerSelection == "rock") {
         if (computerSelection == "rock") {
+            let winner = "none"
             result = "It's a tie!"
-            return result
-        } else {
-            return "none"
+        } else if (computerSelection == "paper") {
+            winner = "computer"
+            result = "You Lose! Paper beats rock!"
+        } else if (computerSelection == "scissors") {
+            winner = "player"
+            result = "You Win! Rock beats scissors!"
         }
-    } else {
-        return "wrong"
+        return result;
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            winner = "player"
+            result = "You win! Paper beats rock!"
+        } else if (computerSelection == "paper") {
+            winner = "none"
+            result = "It's a tie!"
+        } else if (computerSelection == "scissors") {
+            winner = "computer"
+            result = "You Lose! Scissors beats paper!"
+        }
+        return result;
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
+            winner = "computer"
+            result = "You lose! Rock beats scissors!"
+        } else if (computerSelection == "paper") {
+            winner = "player"
+            result = "You win! Scissors beats paper!"
+        } else if (computerSelection == "scissors") {
+            winner = "none"
+            result = "It's a tie!"
+        }
+        return winner;
+    }
+
+    else {
+        return "error"
     }
 }
 
+let whoWins = round(playerSelection, computerSelection)
 console.log(round(playerSelection, computerSelection));
+
+function game() {
+    let computerScore = 0;
+    let playerScore = 0;
+    for (let index = 0; index < 4; index++) {
+        round(playerSelection, computerSelection);
+        if (whoWins == "computer") {
+            computerScore++;
+        } else if (whoWins == "player") {
+            playerScore++;
+        }
+        console.log("computer:" + computerScore + ", player:" + playerScore) 
+        return computerScore, playerScore
+    }
+
+}
+
+console.log(game())
 
