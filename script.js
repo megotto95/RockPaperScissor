c6ad544332b2ca14a66ac972ac663eaef9bcc715
megotto95 <megotto95@gmail.com>
2023-08-10 22:39:10 -0400

    add recalculate loop and other result cases
    If x is 0, recalculates x and added cases for paper and scissors

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 function getComputerChoice() {
-    let x = Math.floor(Math.random() * 10)
+    let x = Math.floor((Math.random() * 10))
+    console.log(x)
+    while (x == 0) {
+        x = Math.floor(Math.random() * 10)
+    }
     console.log(x)
     let comResult;
     if (x >= 7) {
         comResult = "rock"
+        return comResult
+    } else if (x >= 4) {
+        comResult = "paper"
+        return comResult
+    } else if ( x >= 1) {
+        comResult = "scissors"
         return comResult
     }
     
